741f8a9c9ccdedbd11cd465991a038659a619d62
Matt Edwards <m.edwards@bristol.ac.uk>
2020-01-06 17:13:10 +0000

    Start on minimum viable product.
    No tests yet.

--- main.py
+++ main.py
@@ -1,1 +1,127 @@
-print("Hello World!")
+from datetime import datetime as dtime
+from datetime import timedelta
+import json
+
+
+class DayHours():
+    def __init__(self, time_dict):
+        """Initialises a 'DayHours' object from dictionary based data
+
+        Required format:
+            date: dd/mm/yyyy
+            start: hh:mm
+            end: hh:mm
+            lunch_start (optional): hh:mm
+            lunch_end (optional): hh:mm
+
+        Checks the validity of the times:
+                start < end
+                lunch start > start
+                lunch start < end
+                lunch end > lunch start
+                lunch end < end
+        """
+
+        self.date = dtime.strptime(time_dict['date'], "%d/%m/%Y").date()
+        self.start = dtime.strptime(time_dict['start'], "%H:%M")
+        self.end = dtime.strptime(time_dict['end'], "%H:%M")
+
+        # attempt to parse lunch data. If no lunch start time is given, then we
+        # don't require an end either.
+        # Yet if one but not the other is given, this should raise an error
+        try:
+            self.lunch_start = dtime.strptime(time_dict['lunch_start'],
+                                              "%H:%M")
+        except KeyError:
+            self.lunch_start = None
+
+        try:
+            self.lunch_end = dtime.strptime(time_dict['lunch_end'],
+                                            "%H:%M")
+            # upon succesful parsing of lunch end, we should check if we
+            # have a lunch start
+            if self.lunch_start is None:
+                raise KeyError("Lunch end found, but no lunch start")
+        except KeyError:
+            if self.lunch_start is not None:
+                # there is a start, but no end
+                raise KeyError("Lunch end not found")
+            # otherwise, they both don't exist so just continue.
+
+        # Now we need to check the times are valid and make sense
+        if self.start > self.end:
+            raise ValueError("Day ended before it started")
+
+        if self.lunch_start is not None:
+            if self.lunch_start < self.start:
+                raise ValueError("Lunch started before day started")
+            if self.lunch_start > self.end:
+                raise ValueError("Lunch started after day ended")
+            if self.lunch_end < self.lunch_start:
+                raise ValueError("Lunch ended before it started")
+            if self.lunch_end > self.end:
+                raise ValueError("Lunch ended after the day ended")
+
+    def lunch_duration(self):
+        """Return the length of the lunch break for the day
+        """
+        if self.lunch_start is None:
+            return timedelta(seconds=0)
+        else:
+            return self.lunch_end - self.lunch_start
+
+    def work_hours(self):
+        """Return the number of hours worked, accounting for lunch
+        """
+        return self.end - self.start - self.lunch_duration()
+
+
+def parse_work_json_data(filename):
+    """Parses the specified JSON file to gather the various start and end times.
+
+    Required format:
+        "data": [
+            {
+                "date": "dd/mm/yyyy"
+                "start": "hh:mm"
+                "end": "hh:mm"
+                "lunch_start": (optional): "hh:mm"
+                "lunch_end": (optional): "hh:mm"
+            },
+            {
+                "date": "dd/mm/yyyy"
+                "start": "hh:mm"
+                "end": "hh:mm"
+                "lunch_start": (optional): "hh:mm"
+                "lunch_end": (optional): "hh:mm"
+            },
+            ...
+        ]
+
+
+    Arguments:
+        filename {string} -- The location of the json file containing
+                             start and end times
+
+    Returns:
+        all_days {list, DayHours} -- a list of DayHours objects
+    """
+
+    with open(filename) as file:
+        raw_data = json.load(file)
+
+    # convert dictionary data into more convenient form
+    all_days = []
+    for day in raw_data["data"]:
+        all_days.append(DayHours(day))
+
+    return all_days
+
+
+if __name__ == "__main__":
+    data = parse_work_json_data('mock_data.json')
+
+    for day in data:
+        print(day.date)
+        print(day.lunch_duration())
+        print(day.work_hours())
